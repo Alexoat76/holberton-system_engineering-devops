c57efde971802223f4ccd0da9d603864be9b6685
Alexoat76 <3915@holbertonschool.com>
2022-04-19 10:00:56 -0500

    Using Puppet| Install puppet-lint :heavy_check_mark:

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# File:   1-install_a_package.pp
+# Author: Alex Orland Arévalo Tribaldos
+# email:  <3915@holbertonschool.com>
+
+# Using Puppet, install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
+  }
