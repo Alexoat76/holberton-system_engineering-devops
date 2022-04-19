3fd3ac141fa801028d99e5278f2d409638dbdc85
Alexoat76 <3915@holbertonschool.com>
2022-04-19 10:20:07 -0500

    testing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,3 @@
-# File:   1-install_a_package.pp
-# Author: Alex Orland Arévalo Tribaldos
-# email:  <3915@holbertonschool.com>
-
 # Using Puppet, install puppet-lint
 package { 'puppet-lint':
   ensure   => '2.5.0',
